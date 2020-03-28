1a9d4421919fbbe6027e24924ae0f712457cf918
unknown <ilhom0258@gmail.com>
2020-03-28 19:43:03 +0500

    1st Commit

--- Program.cs
+++ Program.cs
@@ -6,8 +6,54 @@ namespace _5thLesson
     {
         static void Main(string[] args)
         {
-            // Циклы 
+
+            /// Class Work 
             
+            {
+                Console.Write("A:"); 
+                int a = int.Parse(Console.ReadLine());
+                Console.Write("B:"); 
+                int b = int.Parse(Console.ReadLine());
+                int sum = 0; 
+                while ( a <= b ){
+                    sum+=a; 
+                    a++; 
+                } 
+                System.Console.WriteLine("Sum = " + sum);
+            
+            }
+            int tries = 5; 
+            string word = "Ilhom";  
+            int n = 0;  
+            while ( tries > 0 ){
+                
+                bool t = false; 
+                Console.Write("Letter:");
+                char choice = char.Parse(Console.ReadLine());
+                string q = choice.ToString(); 
+                q = q.ToLower(); 
+                choice 
+                = char.Parse(q); 
+                string wordInput = word.ToLower(); 
+                for ( int i = 0; i < word.Length; i++ ){
+                    if ( word.Contains(choice) ){
+                        
+                        System.Console.WriteLine("Right!!");
+                        n++; 
+                        t = true;
+                        break;        
+                    } 
+                }
+                if ( word.Length == n ){
+                    System.Console.WriteLine($"You did it.... The word is {word}");
+                    break; 
+                }
+                if ( !t ){
+                    tries--; 
+                }
+            }
+            Console.ReadKey(); 
+
         }
     }
 }
